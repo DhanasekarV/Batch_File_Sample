4e5e7b6c68ca0a9583b681095ccfffad882ab72a
DhanasekarV <vasu.dhan14@gmail.com>
2020-09-25 11:38:51 +0530

    Update chrome.bat

--- chrome.bat
+++ chrome.bat
@@ -1,7 +1,7 @@
 @ECHO off
-
+:: Change directory to chrome where the .exe is present.
 cd "\Program Files\Google\Chrome\Application"
-
+:: Excecute the Chrome application
 chrome.exe
-
+:: Change back to desktop directory.
 cd \Users\$<User_Name>\Desktop
